fe877a2ab89445b2348290a8020943bd2b94261d
t9950 <101507876+t9950@users.noreply.github.com>
2022-03-13 15:36:30 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+{
+  "name": "hardhat-project",
+  "devDependencies": {
+    "@nomiclabs/hardhat-ethers": "^2.0.4",
+    "@nomiclabs/hardhat-waffle": "^2.0.2",
+    "@nomiclabs/hardhat-web3": "^2.0.0",
+    "chai": "^4.3.6",
+    "ethereum-waffle": "^3.4.0",
+    "ethers": "^5.5.4",
+    "hardhat": "^2.8.3",
+    "solidity-coverage": "^0.7.19",
+    "web3": "^1.7.0"
+  },
+  "dependencies": {
+    "dotenv": "^16.0.0"
+  }
+}
